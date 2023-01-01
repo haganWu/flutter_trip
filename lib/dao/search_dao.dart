a348b1b5ba19ee5369155ff29c403e695c619c9f
WuHaiheng <wuhh@csco.com.cn>
2023-01-01 16:32:53 +0800

    首页 -- 搜索接口Model和Dao封装

---
+++ lib/dao/search_dao.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_trip/model/search_model.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+// const SEARCH_URL = "https://m.ctrip.com/restapi/h5api/globalsearch/search?source=mobileweb&action=mobileweb&keyword=";
+
+class SearchDao {
+  static Future<SearchModel> fetch(String searchUrl) async {
+    var url = Uri.parse(searchUrl);
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      Utf8Decoder utf8decoder = const Utf8Decoder(); // 防止中文乱码
+      var result = json.decode(utf8decoder.convert(response.bodyBytes));
+      return SearchModel.fromJson(result);
+    } else {
+      throw Exception('Failed to load search_page.json');
+    }
+  }
+}
--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -1,7 +1,12 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_trip/dao/search_dao.dart';
+import 'package:flutter_trip/model/search_model.dart';
 import 'package:flutter_trip/widget/search_bar.dart';
 
 class SearchPage extends StatefulWidget {
+
   const SearchPage({Key? key}) : super(key: key);
 
   @override
@@ -9,6 +14,26 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
+
+  final String SEARCH_URL = "https://m.ctrip.com/restapi/h5api/globalsearch/search?source=mobileweb&action=mobileweb&keyword=";
+  String showText = "11";
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  _getData() async {
+    try {
+      SearchModel model = await SearchDao.fetch(SEARCH_URL);
+      print(json.encode(model.data![0]));
+      setState(() {
+        showText = json.encode(model.data![0]);
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,7 +50,14 @@ class _SearchPageState extends State<SearchPage> {
                 rightButtonClick: _rightButtonClick,
                 speakClick: _speakClick,
                 inputBoxClick: _inputBoxClick,
-                onChanged: _onTextChanged)
+                onChanged: _onTextChanged),
+            InkWell(
+              onTap: (){
+                _getData();
+              },
+              child: Text("Get"),
+            ),
+            Text(showText)
           ],
     ));
   }
